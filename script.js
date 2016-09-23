8086926855116e262e263fc23ba88642ddb8db5a
sepandhaghighi <sepand.haghighi@yahoo.com>
2016-09-23 23:08:26 +0330

    minor edit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
-var image_arr = ["files/Gallery/1.jpg"
+var image_arr = ["files/Gallery/1.jpg",
+                 "files/Gallery/2.jpg"
                  ];
+
+
                 
 var index = 0;
 var time_1;
@@ -8,22 +11,25 @@ var time_2;
 
 function next() {
     var slider = document.getElementById("slidebar");
+    var text = document.getElementById("image_text");
     index=index+1;
     if (index==image_arr.length){
         index=0;
     }
     slider.src = image_arr[index];
-    
+    text.innerHTML="("+(index+1)+")";
     
 }
 
 function prev() {
     var slider = document.getElementById("slidebar");
+    var text = document.getElementById("image_text");
     index=index-1;
     if (index<0){
         index=image_arr.length-1;
     }
     slider.src = image_arr[index];
+    text.innerHTML="("+(index+1)+")";
     
 }
 function colorchange1(a){

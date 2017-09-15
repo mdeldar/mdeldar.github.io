e58f2c67058b8eee29f430ce45f648d9c78ad120
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-09-15 14:22:17 +0430

    feat : new photo gallery added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+var image_arr = ["files/Gallery/1.jpg",
+                 "files/Gallery/2.jpg",
+                 "files/Gallery/3.jpg",
+                 "files/Gallery/4.jpg",
+                 "files/Gallery/5.jpg",
+                 "files/Gallery/6.jpg",
+                 "files/Gallery/7.jpg",
+                 "files/Gallery/8.jpg",
+                 "files/Gallery/9.jpg"
+                 
+                 ];
+
+
+                
+var index = 0;
+var time_1;
+var time_2;
+
+function next() {
+    var slider = document.getElementById("slidebar");
+    index=index+1;
+    if (index==image_arr.length){
+        index=0;
+    }
+    slider.src = image_arr[index];
+    
+}
+
+function prev() {
+    var slider = document.getElementById("slidebar");
+    index=index-1;
+    if (index<0){
+        index=image_arr.length-1;
+    }
+    slider.src = image_arr[index];
+    
+}
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     var menu=document.getElementById("menu");
--- script.js
+++ script.js
@@ -1,37 +1,5 @@
 "use strict";
-var image_arr = ["files/Gallery/1.jpg",
-                 "files/Gallery/2.jpg"
-                 ];
 
-
-                
-var index = 0;
-var time_1;
-var time_2;
-
-function next() {
-    var slider = document.getElementById("slidebar");
-    var text = document.getElementById("image_text");
-    index=index+1;
-    if (index==image_arr.length){
-        index=0;
-    }
-    slider.src = image_arr[index];
-    text.innerHTML="("+(index+1)+")";
-    
-}
-
-function prev() {
-    var slider = document.getElementById("slidebar");
-    var text = document.getElementById("image_text");
-    index=index-1;
-    if (index<0){
-        index=image_arr.length-1;
-    }
-    slider.src = image_arr[index];
-    text.innerHTML="("+(index+1)+")";
-    
-}
 function colorchange1(a){
     var but=document.getElementsByTagName("button");
     if (a==1) {
